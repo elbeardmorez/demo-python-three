b0fee310141f1327b74d1ca61ca6d0f033a6dd31
Pete Beardmore <pete.beardmore@msn.com>
2019-10-30 12:31:45 +0000

    [add] tornado http client
    -fetch target pages

--- spider.py
+++ spider.py
@@ -1,5 +1,7 @@
 import sys
 import argparse
+import asyncio
+from tornado import httpclient
 
 verbose = 0
 
@@ -9,12 +11,40 @@ def debug(*args):
         print("[debug]", *args)
 
 
-def crawl(target):
+def dump_response(response):
+    print("headers:")
+    for (k, v) in response.headers.get_all():
+        print(f"{k}: {v}")
+    print("cookies:")
+    print('\n'.join(response.headers.get_list('set-cookie')))
+    print("body:")
+    print(response.body.decode())
 
+
+async def crawl(root):
+
+    httpclient.AsyncHTTPClient.configure(
+        "tornado.curl_httpclient.CurlAsyncHTTPClient")
+    client = httpclient.AsyncHTTPClient(force_instance=True)
+
+    # authentication
+    target = root + '/login.php'
     debug(f"crawling '{target}'")
 
+    response = await client.fetch(target)
+    dump_response(response)
 
-def spider():
+    # sql-injection compromise
+    target = root + '/vulnerabilities/sqli/'
+    debug(f"crawling '{target}'")
+
+    response = await client.fetch(target)
+    dump_response(response)
+
+    client.close()
+
+
+async def spider():
 
     parser = argparse.ArgumentParser(
         description='A spider that crawles')
@@ -37,8 +67,8 @@ def spider():
     global verbose
     verbose = args.verbose
 
-    crawl(target)
+    await crawl(target)
 
 
 if __name__ == "__main__":
-    spider()
+    asyncio.run(spider())
