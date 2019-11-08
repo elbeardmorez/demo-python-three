66cac28aae8474f8c3294004cd7c0f0ea4142752
Pete Beardmore <pete.beardmore@msn.com>
2019-11-08 14:02:42 +0000

    [mod] whitespace changes only
    -supplemental to previous commit

--- src/api.py
+++ src/api.py
@@ -97,14 +97,14 @@ async def spdr_inject(url, state):
 
 def spdr_next_url(state):
     next_ = None
-        with state.mutex['unprocessed']:
+    with state.mutex['unprocessed']:
         while len(state.url_pools['unprocessed']) > 0:
             next_ = state.url_pools['unprocessed'].popleft()
-        if next_ not in state.url_pools['processed'] and \
-           next_ not in state.url_pools['processing']:
-            with state.mutex['processing']:
-                state.url_pools['processing'][next_] = 1
-            break
+            if next_ not in state.url_pools['processed'] and \
+               next_ not in state.url_pools['processing']:
+                with state.mutex['processing']:
+                    state.url_pools['processing'][next_] = 1
+                break
 
     return next_
 
